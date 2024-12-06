d61464145e1129e36981a00298c379c988e87683
Sebastian Krepela <sebous@gmail.com>
2024-12-06 14:46:48 +0100

    d6 brute forced 🚀

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -4,9 +4,7 @@ use advent_of_code::grid::{Coord, Grid};
 
 advent_of_code::solution!(6);
 
-const directions: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
-
-const guard: [char; 4] = ['^', '>', '⌄', '<'];
+const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
 fn parse(input: &str) -> (Grid<char>, Coord) {
     let mut start: (usize, usize) = (0, 0);
@@ -30,15 +28,15 @@ fn parse(input: &str) -> (Grid<char>, Coord) {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (mut grid, start) = parse(input);
+    let (grid, start) = parse(input);
 
     let (mut x, mut y) = start;
     let mut dir_index = 0;
-    let mut movements = HashSet::from([(x, y)]);
+    let mut visited = HashSet::from([(x, y)]);
 
     loop {
-        let x1 = x.checked_add_signed(directions[dir_index].0);
-        let y1 = y.checked_add_signed(directions[dir_index].1);
+        let x1 = x.checked_add_signed(DIRECTIONS[dir_index].0);
+        let y1 = y.checked_add_signed(DIRECTIONS[dir_index].1);
 
         // exits the map
         if y1.is_none_or(|y| y >= grid.height) || x1.is_none_or(|x| x >= grid.width) {
@@ -49,42 +47,65 @@ pub fn part_one(input: &str) -> Option<u32> {
         if let Some(ch) = grid.map.get(&(x1, y1)) {
             match ch {
                 '.' => {
-                    grid.map.insert((x, y), '.');
-                    grid.map.insert((x1, y1), guard[dir_index]);
                     x = x1;
                     y = y1;
-                    movements.insert((x1, y1));
+                    visited.insert((x1, y1));
                 }
                 '#' => {
                     dir_index = (dir_index + 1) % 4;
-                    grid.map.insert((x, y), guard[dir_index]);
                 }
                 _ => unimplemented!("UNKNOWN char"),
             }
         }
-        // println!("{}", grid);
     }
 
-    Some(movements.len() as u32)
+    Some(visited.len() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (mut grid, start) = parse(input);
+
+    let mut count = 0;
+    let mut movements: HashSet<(usize, usize, usize)> = HashSet::with_capacity(10000);
+
+    for (c, _) in grid.map.clone().iter().filter(|(_, ch)| **ch == '.') {
+        grid.map.insert(*c, '#');
+        let mut dir_index = 0;
+        let (mut x, mut y) = start;
+        movements.clear();
+        movements.insert((x, y, dir_index));
+
+        loop {
+            let x1 = x.checked_add_signed(DIRECTIONS[dir_index].0);
+            let y1 = y.checked_add_signed(DIRECTIONS[dir_index].1);
+
+            // exits the map
+            if y1.is_none_or(|y| y >= grid.height) || x1.is_none_or(|x| x >= grid.width) {
+                break;
+            }
+            let (x1, y1) = (x1.unwrap(), y1.unwrap());
+
+            if movements.contains(&(x1, y1, dir_index)) {
+                count += 1;
+                break;
+            }
+
+            if let Some(ch) = grid.map.get(&(x1, y1)) {
+                match ch {
+                    '.' | '^' => {
+                        x = x1;
+                        y = y1;
+                        movements.insert((x1, y1, dir_index));
+                    }
+                    '#' => {
+                        dir_index = (dir_index + 1) % 4;
+                    }
+                    _ => unimplemented!("UNKNOWN char"),
+                }
+            }
+        }
+        grid.map.insert(*c, '.');
+    }
+
+    Some(count)
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_part_one() {
-//         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-//         assert_eq!(result, None);
-//     }
-
-//     #[test]
-//     fn test_part_two() {
-//         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-//         assert_eq!(result, None);
-//     }
-// }
