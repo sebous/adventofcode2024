47f68d358de880a8d608c3d3d3480cbcb58b3af7
Sebastian Krepela <sebous@gmail.com>
2024-12-09 15:21:11 +0100

    cleanup

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -133,20 +133,3 @@ pub fn part_two(input: &str) -> Option<u32> {
     }
     Some(antinodes.len() as u32)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
